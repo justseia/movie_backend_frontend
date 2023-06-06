091f5460bf13d7ae51cd73474be2a0b8e116f439
Seiitmurat Kalmurat <seia@MacBook-Air-Seiitmurat.local>
2023-06-06 07:31:00 +0600

    update

--- app/Http/Controllers/Profile/UpdateController.php
+++ app/Http/Controllers/Profile/UpdateController.php
@@ -9,6 +9,19 @@ class UpdateController extends Controller
 {
     public function __invoke(Request $request)
     {
+        if ($request->hasFile('image')) {
+            $photo = $request->file('photo');
+            $photo->store('public/photos');
+            $user = auth()->user()->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'age' => $request->age,
+                'instagram' => $request->instagram,
+                'university' => $request->university,
+                'hobbi' => $request->hobbi,
+                'avatar' => $photo,
+            ]);
+        }
         $user = auth()->user()->update([
             'name' => $request->name,
             'email' => $request->email,
